518cfa329cdefaaa03c1ce4368317b7836a3ba5a
mkiwoong <mgu6636@gmail.com>
2024-04-01 14:25:26 +0900

    UI구상

---
+++ cafeKiosk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cafeKiosk
+//
+//  Created by 문기웅 on 4/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var categorySegmentControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
