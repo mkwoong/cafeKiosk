0ef43793316bbcacba92d333645b30c764f3fcac
mkiwoong <mgu6636@gmail.com>
2024-04-02 13:38:37 +0900

    셀에 메뉴이름 넣기

--- cafeKiosk/MenuData.swift
+++ cafeKiosk/MenuData.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+struct MenuData {
+    let name: String
+    let price: Int
+    
+    init(name: String, price: Int) {
+        self.name = name
+        self.price = price
+    }
+}
+
+let ameri = MenuData(name: "아메리카노", price: 4500)
+let latte = MenuData(name: "카페라떼", price: 5000)
+let caramel = MenuData(name: "카라멜마끼아또", price: 5500)
+let dolce = MenuData(name: "돌체라떼", price: 5500)
+
+
+let lemon = MenuData(name: "레몬에이드", price: 5500)
+let grapefruit = MenuData(name: "자몽에이드", price: 5500)
+let matcha = MenuData(name: "말차라떼", price: 6000)
+let mango = MenuData(name: "망고스무디", price: 6000)
+
+
+let chocoCake = MenuData(name: "초코케이크", price: 5500)
+let strawberryCake = MenuData(name: "딸기케이크", price: 5500)
+let cheeseCake = MenuData(name: "바스크 치즈케이크", price: 5500)
+
+
--- cafeKiosk/ViewController.swift
+++ cafeKiosk/ViewController.swift
@@ -9,14 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var categorySegmentControl: UISegmentedControl!
+    @IBOutlet weak var menuCollectionView: UICollectionView!
+    
+    var coffeeMenuList:[MenuData] = [ameri,latte,caramel,dolce]
+    var beverageMenuList:[MenuData] = [lemon,grapefruit,matcha,mango]
+    var foodMenuList:[MenuData] = [chocoCake,strawberryCake,cheeseCake]
+    
+    var menuList:[MenuData] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-    @IBAction func tappedCategory(_ sender: Any) {
+
+    @IBAction func tappedCategory(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            menuList = coffeeMenuList
+        case 1:
+            menuList = beverageMenuList
+        case 2:
+            menuList = foodMenuList
+        default:
+            break
+        }
+        menuCollectionView.reloadData()
     }
     
 
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return menuList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = menuCollectionView.dequeueReusableCell(withReuseIdentifier: "MenuCell", for: indexPath) as! MenuCell
+        cell.backgroundColor = .green
+        cell.menuName?.text = menuList[indexPath.row].name
+        return cell
+    }
+    
+    
+}
+
+class MenuCell:UICollectionViewCell {
+    
+    @IBOutlet weak var menuName: UILabel!
+    
+}
